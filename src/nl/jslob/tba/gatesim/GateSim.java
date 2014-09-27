d0d393e0b42e15d1501296216b1222b6c786b0fd
Joris Slob <jorisslob@gmail.com>
2014-09-27 15:18:54 +0200

    Tried to refactor the software to make everything neater

---
+++ src/nl/jslob/tba/gatesim/GateSim.java
@@ -0,0 +1,18 @@
+package nl.jslob.tba.gatesim;
+
+public class GateSim {
+
+	public static void main(String[] args) {
+		// Gate Simulator
+		
+		// Simulator Object that deals with time and registers components
+		// Components include TruckActivity, Gates, Transit, Stacks and Queues
+		// The Simulator Object is repsonsible to check if there is still activity in 
+		// its components and ending the simulation with a statistics overview.
+		
+		
+		
+
+	}
+
+}
