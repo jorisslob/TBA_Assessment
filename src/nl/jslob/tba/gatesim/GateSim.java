32b31f6020a95d0f5572f05799d7e5e0d1f49160
Joris Slob <jorisslob@gmail.com>
2014-09-27 15:29:35 +0200

    Split the simulator to a seperate class

--- src/nl/jslob/tba/gatesim/GateSim.java
+++ src/nl/jslob/tba/gatesim/GateSim.java
@@ -1,21 +1,14 @@
 package nl.jslob.tba.gatesim;
 
+import nl.jslob.tba.gatesim.simulator.Simulator;
+
 public class GateSim {
 
 	public static void main(String[] args) {
-		// Gate Simulator
-		
-		// Simulator Object that deals with time and registers components
-		// Components include TruckActivity, Gates, Transit, Stacks and Queues
-		// The Simulator Object is responsible to check if there is still activity in 
-		// its components and ending the simulation with a statistics overview.
-		
-		// In this particular Simulator, only two parameters are important:
-		// (entry lanes and exit lanes). We will measure the performance by adding up
-		// the total queue waiting time of the trucks and the amount of restriction 
-		// violations
-		
-
+		// The main function runs the simulator and requests statistics.
+		// TODO: Make this parallel so we can simulate faster
+		Simulator sim = new Simulator(1,1);
+		sim.run();
+		System.out.println(sim.getStatistics());
 	}
-
 }
--- src/nl/jslob/tba/gatesim/simulator/Simulator.java
+++ src/nl/jslob/tba/gatesim/simulator/Simulator.java
@@ -1,5 +1,43 @@
 package nl.jslob.tba.gatesim.simulator;
 
 public class Simulator {
+	// Simulator Object that deals with time and registers components
+	// Components include TruckActivity, Gates, Transit, Stacks and Queues
+	// The Simulator Object is responsible to check if there is still activity in 
+	// its components and ending the simulation with a statistics overview.
+	
+	// In this particular Simulator, only two parameters are important:
+	// (entry lanes and exit lanes). We will measure the performance by adding up
+	// the total queue waiting time of the trucks and the amount of restriction 
+	// violations
+
+	/**
+	 * Creates a Simulator with a specified number of entry and exit lanes.
+	 * @param entry number of entry lanes
+	 * @param exit number of exit lanes
+	 */
+	public Simulator(int entry, int exit) {
+		// Needs to setup the harbor with the correct components.
+	}
+
+	public void run() {
+		// Main loop of the simulator
+		while(isActive()) {
+			step();
+		}
+	}
+
+	private void step() {
+		// Nothing to do yet
+	}
+
+	private boolean isActive() {
+		// Nothing happening, so not active
+		return false;
+	}
+
+	public String getStatistics() {
+		return "Nothing simulated";
+	}
 
 }
