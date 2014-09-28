85258d46e8d6777166d9039f29b9c6dcfd023c87
Joris Slob <jorisslob@gmail.com>
2014-09-28 21:55:07 +0200

    Checkstyle on GammaDistributionRate

--- src/nl/jslob/tba/gatesim/util/GammaDistributionRate.java
+++ src/nl/jslob/tba/gatesim/util/GammaDistributionRate.java
@@ -2,17 +2,49 @@ package nl.jslob.tba.gatesim.util;
 
 import org.apache.commons.math3.distribution.GammaDistribution;
 
+/**
+ * GammaDistributionRate is a wrapper around the Apache commons
+ * GammaDistribution. In the Apache commons, it is assumed that the scale is
+ * given, instead of the rate as parameter.
+ *
+ * @author jslob
+ *
+ */
 public class GammaDistributionRate {
 
-	GammaDistribution gd;
+    /**
+     * Number of seconds in a minute, physical constant.
+     */
+    private static final int SECONDSINMINUTE = 60;
 
-	public GammaDistributionRate(int alpha, int beta) {
-		double inv_beta = 1.0 / (float) beta;
-		gd = new GammaDistribution(alpha, inv_beta);
-	}
+    /**
+     * This is the Apache commons GammaDistribution that actually does all the
+     * work.
+     */
+    private final GammaDistribution gd;
 
-	public long getValueInSeconds() {
-		long seconds = (long) (gd.sample() * 60);
-		return seconds;
-	}
+    /**
+     * Initialize a GammaDistributionRate where the alpha is the shape parameter
+     * and beta the rate parameter.
+     *
+     * @param alpha
+     *            Shape parameter of the Gamma Distribution.
+     * @param beta
+     *            Rate paramter of the Gamma Distribution.
+     */
+    public GammaDistributionRate(final int alpha, final int beta) {
+        double invBeta = 1.0 / beta;
+        gd = new GammaDistribution(alpha, invBeta);
+    }
+
+    /**
+     * The GammaDistribution now returns values in minutes, but we want to be
+     * more accurate. Here we return the values in seconds.
+     *
+     * @return Gamma Distribution Values in seconds.
+     */
+    public final long getValueInSeconds() {
+        long seconds = (long) (gd.sample() * SECONDSINMINUTE);
+        return seconds;
+    }
 }
