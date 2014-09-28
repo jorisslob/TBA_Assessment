3f93759cd48a81cdc83c29df9b0383b54ddf780b
Joris Slob <jorisslob@gmail.com>
2014-09-28 23:18:05 +0200

    Checkstyle Statistics

--- src/nl/jslob/tba/gatesim/simulator/Statistics.java
+++ src/nl/jslob/tba/gatesim/simulator/Statistics.java
@@ -4,40 +4,78 @@ package nl.jslob.tba.gatesim.simulator;
  * Statistics is an object that holds the results of the simulation and can
  * report back in the end. In this simulation, the total queue time of trucks
  * and the queue violations should show up.
- * 
+ *
  * @author jslob
  */
 public class Statistics {
-	int num_of_trucks;
-	long queueTime;
-	boolean queueViolation;
 
-	public Statistics() {
-		queueTime = 0;
-		queueViolation = false;
-		num_of_trucks = 0;
-	}
+    /**
+     * Number of trucks that went through the entire simulation.
+     */
+    private int truckNumber;
 
-	public void addTruck(Truck t) {
-		num_of_trucks++;
-		if(t.getQueueSeconds()<0) {
-			throw new IllegalStateException("Cannot spend negative time in queue");
-		}
-		queueTime += t.getQueueSeconds();
-		if(t.getLongQueue()) {
-			queueViolation = true;
-		}
-	}
+    /**
+     * Total time that all the trucks spent in queues.
+     */
+    private long queueTime;
 
-	public int getNumOfTrucks() {
-		return num_of_trucks;
-	}
+    /**
+     * Has there been any queue violation in the entire simulation?
+     */
+    private boolean queueViolation;
 
-	public long getTotalQueueTime() {
-		return queueTime;
-	}
+    /**
+     * Constructor for the Statistics class, starts all statistics at zero.
+     */
+    public Statistics() {
+        queueTime = 0;
+        queueViolation = false;
+        truckNumber = 0;
+    }
 
-	public boolean getQueueViolation() {
-		return queueViolation;
-	}
+    /**
+     * Add a truck to the statistics. The truck holds the information that has
+     * to be added to the overall statistics.
+     *
+     * @param t
+     *            Truck that went through all components.
+     */
+    public final void addTruck(final Truck t) {
+        truckNumber++;
+        if (t.getQueueSeconds() < 0) {
+            throw new IllegalStateException(
+                    "Cannot spend negative time in queue");
+        }
+        queueTime += t.getQueueSeconds();
+        if (t.getLongQueue()) {
+            queueViolation = true;
+        }
+    }
+
+    /**
+     * Number of trucks that went through all the components.
+     *
+     * @return Number of trucks that went through all the components
+     */
+    public final int getNumOfTrucks() {
+        return truckNumber;
+    }
+
+    /**
+     * Checks if there has been any violation of queue length in the simulation.
+     *
+     * @return true if there has been a violation of queue length restriction
+     */
+    public final boolean getQueueViolation() {
+        return queueViolation;
+    }
+
+    /**
+     * Get the total time that all the trucks spent waiting in a queue.
+     *
+     * @return total time that all trucks spent waiting in a queue
+     */
+    public final long getTotalQueueTime() {
+        return queueTime;
+    }
 }
