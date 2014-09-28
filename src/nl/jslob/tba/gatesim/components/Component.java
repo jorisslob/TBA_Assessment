91508d15a91863330d8411bf765ca3fcdf444ab1
Joris Slob <jorisslob@gmail.com>
2014-09-28 17:54:55 +0200

    Checkstyle fixes on Component.java

--- src/nl/jslob/tba/gatesim/components/Component.java
+++ src/nl/jslob/tba/gatesim/components/Component.java
@@ -5,13 +5,28 @@ import nl.jslob.tba.gatesim.simulator.Truck;
 /**
  * A component is something that modifies the schedule of a truck. Any time a
  * truck has a new ETA it has been caused by a component.
- * 
+ *
  * @author jslob
  *
  */
 public interface Component {
 
-	public abstract void acceptTruck(Truck t);
+    /**
+     * Function gets called when the Schedule class determines that a truck
+     * should go to this component. The component MUST change the location in
+     * the truck and then it can deal with the truck as it wants.
+     *
+     * @param t
+     *            is the truck that the component receives
+     */
+    void acceptTruck(Truck t);
 
-	public abstract void releaseTruck(Truck t);
+    /**
+     * Function gets called when the Schedule class determines that a truck
+     * should leave this component.
+     *
+     * @param t
+     *            is the truck that will leave the component
+     */
+    void releaseTruck(Truck t);
 }
---
+++ src/nl/jslob/tba/gatesim/components/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package holds all the components that can occur in a harbor. They
+ * have to implement the Component interface.
+ */
+package nl.jslob.tba.gatesim.components;
