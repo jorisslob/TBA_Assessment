dcb8b28b2d491ba67a421fe1153bc9e02882a154
Joris Slob <jorisslob@gmail.com>
2014-09-28 16:28:18 +0200

    Corrected all the checkstyle mistakes in GateSim.java

--- src/nl/jslob/tba/gatesim/GateSim.java
+++ src/nl/jslob/tba/gatesim/GateSim.java
@@ -2,28 +2,79 @@ package nl.jslob.tba.gatesim;
 
 import nl.jslob.tba.gatesim.simulator.Simulator;
 
-public class GateSim {
-	private static int SIMULATIONS = 100;
+/**
+ * GateSim is the entrypoint of the program. It runs simulations as descibed
+ * in ASSIGNMENT.md. The class determines how many times the simulation will
+ * run and with what parameters. The results are sent to the System standard
+ * output. The output has the following format:
+ * ENTRY_LANES,EXIT_LANES,TOTAL_WAIT_TIME,VIOLATIONS?
+ *
+ * Where ENTRY_LANES is the number of lanes were simulated for the entry gate,
+ * EXIT_LANES the number of lanes for the exit gate, TOTAL_WAIT_TIME is the
+ * sum of the time that all trucks spent in queues and VIOLATIONS? is a
+ * boolean that notes if any of the queues exceeded 30 trucks.
+ *
+ * Constant parameters that can be changed to change the behavior of the
+ * program:
+ * <ul>
+ * <li>SIMULATIONS: Determines how many times each simulation has to run.
+ * <li>ENTRY_MAX: Determines how many entry lanes are maximally tested.
+ * <li>EXIT_MAX: Determines how many exit lanes are maximally tested.
+ * </ul>
+ *
+ * @author jslob
+ *
+ */
+public final class GateSim {
+    /**
+     * SIMULATIONS determines how many times every simulation must run for
+     * statistical significance.
+     */
+    private static final int SIMULATIONS = 100;
+    /**
+     * ENTRY_MAX determines up to how many entry lanes must be simulated.
+     */
+    private static final int ENTRY_MAX = 20;
+    /**
+     * EXIT_MAX determines up to how many exit lanes must be simulated.
+     */
+    private static final int EXIT_MAX = 20;
 
-	public static void main(String[] args) {
-		// The main function runs the simulator and requests statistics.
-		// TODO: Make this parallel so we can simulate faster
-		for(int i=1; i<20; i++) {
-			for(int j=1; j<20; j++) {
-				long wait = 0;
-				boolean violation = false;
-				int rounds = 0;
-				for(int k=0; k<SIMULATIONS; k++) {
-					Simulator sim = new Simulator(i, j);
-					sim.run();
-					wait += sim.getWaitTime();
-					violation = sim.getViolation();
-					rounds++;
-					if(violation) break;
-				}
-				double avg = (double) wait / (double) rounds;
-				System.out.println(i + "," + j + "," + avg + "," + violation);
-			}
-		}
-	}
+    /**
+     * Main is the entry function of our program. It will run through all
+     * number of entry and exit lane possibilities and report back which ones
+     * violate the max queue rule and how much time the trucks spend waiting
+     * in queues.
+     *
+     * @param args Arguments to this program are ignored.
+     */
+    public static void main(final String[] args) {
+        // The main function runs the simulator and requests statistics.
+        for (int i = 1; i < ENTRY_MAX; i++) {
+            for (int j = 1; j < EXIT_MAX; j++) {
+                long wait = 0;
+                boolean violation = false;
+                int rounds = 0;
+                for (int k = 0; k < SIMULATIONS; k++) {
+                    Simulator sim = new Simulator(i, j);
+                    sim.run();
+                    wait += sim.getWaitTime();
+                    violation = sim.getViolation();
+                    rounds++;
+                    if (violation) {
+                        break;
+                    }
+                }
+                double avg = (double) wait / (double) rounds;
+                System.out.println(i + "," + j + "," + avg + "," + violation);
+            }
+        }
+    }
+
+    /**
+     * Private constructor to avoid instantiation.
+     */
+    private GateSim() {
+        // Class should not be instantiated
+    }
 }
---
+++ src/nl/jslob/tba/gatesim/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package is the container of the entire project. It contains only a
+ * single class, which is the entry point of the program.
+ */
+package nl.jslob.tba.gatesim;
