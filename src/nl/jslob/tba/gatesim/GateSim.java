63edd2d1eb7eab67adc87f845c75f628ac4b9e0b
Joris Slob <jorisslob@gmail.com>
2014-09-28 12:01:01 +0200

    Revert the original experiment back to loop over 1-20 in both entry and exit lanes

--- src/nl/jslob/tba/gatesim/GateSim.java
+++ src/nl/jslob/tba/gatesim/GateSim.java
@@ -7,8 +7,8 @@ public class GateSim {
 	public static void main(String[] args) {
 		// The main function runs the simulator and requests statistics.
 		// TODO: Make this parallel so we can simulate faster
-		for(int i=7; i<15; i++) {
-			for(int j=3; j<10; j++) {
+		for(int i=1; i<20; i++) {
+			for(int j=1; j<20; j++) {
 				Simulator sim = new Simulator(i, j);
 				sim.run();
 				System.out.println(i + "," + j + "," + sim.getStatistics());		
