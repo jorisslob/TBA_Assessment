57a2ada94b04bade8dc8af6746f8867dae46cec6
Joris Slob <jorisslob@gmail.com>
2014-09-28 17:16:54 +0200

    Ran checkstyle over Simulator and cleaned it up

--- src/nl/jslob/tba/gatesim/simulator/Simulator.java
+++ src/nl/jslob/tba/gatesim/simulator/Simulator.java
@@ -16,109 +16,124 @@ import nl.jslob.tba.gatesim.util.ExcelTruckReader;
  * include TruckActivity, Gates, Transit, Stacks and Queues The Simulator Object
  * is responsible to check if there is still activity in its components and
  * ending the simulation with a statistics overview.
- * 
+ *
  * In this particular Simulator, only two parameters are important: (entry lanes
  * and exit lanes). We will measure the performance by adding up the total queue
  * waiting time of the trucks and the amount of restriction violations
- * 
+ *
  * @author jslob
  */
 public class Simulator {
 
-	// We choose a LinkedList for this implementation because the order is
-	// important
-	// and we have easy access to first and last elements.
-	LinkedList<Component> components;
+    /**
+     * Components is a list of locations in the harbor that a truck has to visit
+     * in order. In the implementation of the Simulator the components have to
+     * start and finish with a World class. Trucks come from the outside world
+     * and go back to the outside world.
+     *
+     * components is an instance of a LinkedList, because we need access to its
+     * elements by index. An array would also work (even faster), but I choose
+     * this class for readability of the code.
+     */
+    private LinkedList<Component> components;
 
-	// The schedule has all the events that are waiting to happen.
-	Schedule sim_schedule;
+    /**
+     * The schedule has all the events that are waiting to happen.
+     */
+    private Schedule schedule;
 
-	// Statistics object holds the important values for this simulation.
-	Statistics stats;
+    /**
+     * Statistics object holds the important values for this simulation.
+     */
+    private Statistics stats;
 
-	/**
-	 * Creates a Simulator with a specified number of entry and exit lanes.
-	 * 
-	 * @param entry
-	 *            number of entry lanes
-	 * @param exit
-	 *            number of exit lanes
-	 */
-	public Simulator(int entry, int exit) {
-		// Initialize internal structures of the Simulator
-		components = new LinkedList<Component>();
-		sim_schedule = new Schedule();
-		stats = new Statistics();
+    /**
+     * Creates a Simulator with a specified number of entry and exit lanes.
+     *
+     * @param entry
+     *            number of entry lanes
+     * @param exit
+     *            number of exit lanes
+     */
+    public Simulator(final int entry, final int exit) {
+        // Initialize internal structures of the Simulator
+        components = new LinkedList<Component>();
+        schedule = new Schedule();
+        stats = new Statistics();
 
-		// Put the components in place
-		Component world = new World(stats);
-		Component entry_gate = new Gate(entry, sim_schedule, 9, 3);
-		Component exit_gate = new Gate(exit, sim_schedule, 3, 3);
-		Component transit_entry_stack = new Transit(sim_schedule);
-		Component transit_stack_exit = new Transit(sim_schedule);
-		Component stack = new StackModules(10, sim_schedule, 4, 2);
-		components.add(world);
-		components.add(entry_gate);
-		components.add(transit_entry_stack);
-		components.add(stack);
-		components.add(transit_stack_exit);
-		components.add(exit_gate);
-		components.add(world);
+        // Put the components in place. If this simulation is used more often
+        // for different harbors, consider putting the numbers in an external
+        // file.
+        Component world = new World(stats);
+        Component entryGate = new Gate(entry, schedule, 9, 3);
+        Component exitGate = new Gate(exit, schedule, 3, 3);
+        Component transitEntryStack = new Transit(schedule);
+        Component transitStackExit = new Transit(schedule);
+        Component stack = new StackModules(10, schedule, 4, 2);
+        components.add(world);
+        components.add(entryGate);
+        components.add(transitEntryStack);
+        components.add(stack);
+        components.add(transitStackExit);
+        components.add(exitGate);
+        components.add(world);
 
-		// Register the components so the schedule knows them
-		sim_schedule.registerComponents(components);
+        // Register the components so the schedule knows them
+        schedule.registerComponents(components);
 
-		// Get the data from the excel list and start populating the scheduler
-		HashMap<Truck, LocalDateTime> timelist = ExcelTruckReader.read();
-		sim_schedule.addAll(timelist, components.getFirst());
-	}
+        // Get the data from the excel list and start populating the scheduler
+        HashMap<Truck, LocalDateTime> timelist = ExcelTruckReader.read();
+        schedule.addAll(timelist, components.getFirst());
+    }
 
-	public void run() {
-		// Main loop of the simulator
-		while (isActive()) {
-			step();
-		}
-	}
+    /**
+     * getViolation is a way to see if the simulation went into a state that
+     * violated the restriction that no queue should be longer than 30 trucks.
+     *
+     * @return true if there was a violation of the max 30 truck queue length.
+     */
+    public final boolean getViolation() {
+        return stats.getQueueViolation();
+    }
 
-	private void step() {
-		sim_schedule.doNextStep();
-	}
+    /**
+     * getWaitTime is a way to get the total time that trucks had to wait in
+     * queues in the simulation. The answer is given in seconds.
+     *
+     * @return a long that holds the number of seconds all the trucks had to
+     *         wait in queue
+     */
+    public final long getWaitTime() {
+        return stats.getTotalQueueTime();
+    }
 
-	private boolean isActive() {
-		return sim_schedule.isActive();
-	}
+    /**
+     * isActive reports back whether the simulation is still running.
+     *
+     * @return true if the simulation is still running.
+     */
+    private boolean isActive() {
+        return schedule.isActive();
+    }
 
-	public String getStatistics() {
-		/*StringBuilder sb = new StringBuilder();
-		sb.append("Total Queue Time: ");
-		sb.append(stats.getTotalQueueTime());
-		sb.append("\n");
-		sb.append("Total Queue Violations: ");
-		sb.append(stats.getTotalQueueViolations());
-		sb.append("\n");
-		sb.append("Number of Trucks: ");
-		sb.append(stats.getNumOfTrucks());
-		sb.append("\n");
-		return sb.toString();
-		*/
-		StringBuilder sb = new StringBuilder();
-		sb.append(stats.getTotalQueueTime());
-		sb.append(",");
-		if(stats.getQueueViolation()) {
-			sb.append("1");	
-		} else {
-			sb.append("0");
-		}
-		
-		return sb.toString();
-	}
+    /**
+     * run is the main loop of the simulator. While it is active it will take
+     * steps.
+     */
+    public final void run() {
+        // Main loop of the simulator
+        while (isActive()) {
+            step();
+        }
+    }
 
-	public long getWaitTime() {
-		return stats.getTotalQueueTime();
-	}
-
-	public boolean getViolation() {
-		return stats.getQueueViolation();
-	}
+    /**
+     * step is a single step of the simulator. In this case the steps are
+     * entirely driven by the Schedule class. Although this program could be
+     * adapted to make the layout of the harbor dynamic.
+     */
+    private void step() {
+        schedule.doNextStep();
+    }
 
 }
---
+++ src/nl/jslob/tba/gatesim/simulator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package has all the parts of the simulator that are not components of
+ * a harbor. The most important class is the Simulator class. It uses the
+ * Schedule class for keeping track of time and the Statistics class to keep
+ * track of all the results. The Truck class is passed around as a kind of
+ * message object with a real physical meaning.
+ */
+package nl.jslob.tba.gatesim.simulator;
