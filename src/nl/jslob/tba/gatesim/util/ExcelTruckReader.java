f867fb454fc33df52620168f958a5a38d23823d5
Joris Slob <jorisslob@gmail.com>
2014-09-28 21:44:13 +0200

    checkstyle on ExcelTruckReader

--- src/nl/jslob/tba/gatesim/util/ExcelTruckReader.java
+++ src/nl/jslob/tba/gatesim/util/ExcelTruckReader.java
@@ -16,52 +16,106 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
-public class ExcelTruckReader {
-	private static String FILEPATH = "TruckActivity.xlsx";
+/**
+ * ExcelTruckReader is a utility class that reads Trucks from a specific xlsx
+ * file. The Trucks are then returned in a Map with the truck and the time of
+ * arrival.
+ *
+ * @author jslob
+ *
+ */
+public final class ExcelTruckReader {
+    /**
+     * FILEPATH is a constant that points to the relative location of the xlsx
+     * file.
+     */
+    private static final String FILEPATH = "TruckActivity.xlsx";
 
-	public static HashMap<Truck, LocalDateTime> read() {
-		InputStream inp;
-		HashMap<Truck, LocalDateTime> truckmap = new HashMap<Truck, LocalDateTime>();
-		try {
-			inp = new FileInputStream(FILEPATH);
-			Workbook wb = WorkbookFactory.create(inp);
-			Sheet sheet = wb.getSheetAt(0);
-			int rownum = 1;
-			Row row = sheet.getRow(rownum);
-			Cell cell1 = row.getCell(0);
-			Cell cell2 = row.getCell(1);
-			Cell cell3 = row.getCell(2);
-			String date, id, kind;
-			int year, month, day, hour, minute, second;
-			while (cell1 != null) {
-				date = cell1.getStringCellValue();
-				id = cell2.getStringCellValue();
-				kind = cell3.getStringCellValue();
-				year = Integer.parseInt(date.substring(0, 4));
-				month = Integer.parseInt(date.substring(4, 6));
-				day = Integer.parseInt(date.substring(6, 8));
-				hour = Integer.parseInt(date.substring(9, 11));
-				minute = Integer.parseInt(date.substring(11, 13));
-				second = Integer.parseInt(date.substring(13, 15));
-				LocalDateTime time = LocalDateTime.of(year, month, day, hour, minute, second);
-				Truck t = new Truck(id, kind);
-				truckmap.put(t, time);
-				rownum++;
-				row = sheet.getRow(rownum);
-				cell1 = row.getCell(0);
-				cell2 = row.getCell(1);
-				cell3 = row.getCell(2);
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			System.exit(1);
-		} catch (InvalidFormatException e) {
-			e.printStackTrace();
-			System.exit(1);
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-		return truckmap;
-	}
+    /**
+     * pareDateTime is a internal function that parses the date format as found
+     * in the xlsx file to a LocalDateTime Object. Assumption is that the
+     * computer running the application is in the same timezone as the xlsx
+     * file.
+     *
+     * This function contains a lot of magic numbers. If the format of the xlsx
+     * file ever changes, this function will break.
+     *
+     * @param date
+     *            String in format yyyyMMdd_hhmmss
+     * @return A LocalDateTime object that matches the string
+     */
+    private static LocalDateTime parseDateTime(final String date) {
+        int year = Integer.parseInt(date.substring(0, 4));
+        int month = Integer.parseInt(date.substring(4, 6));
+        int day = Integer.parseInt(date.substring(6, 8));
+        int hour = Integer.parseInt(date.substring(9, 11));
+        int minute = Integer.parseInt(date.substring(11, 13));
+        int second = Integer.parseInt(date.substring(13, 15));
+        return LocalDateTime.of(year, month, day, hour, minute, second);
+    }
+
+    /**
+     * read is a function that reads a file (FILEPATH) parses it and returns a
+     * HashMap where the keys are Trucks and the values are LocalDateTime
+     * objects. For this function we assume that the file has no errors and
+     * there are no duplicate trucks in the file.
+     *
+     * The file is expected to have the following structure:
+     * <ul>
+     * <li>Column A: A string denoting a datetime in format yyyyMMdd_hhmmss
+     * <li>Column B: A string denoting the id of a truck
+     * <li>Column C: A string denoting whether the truck is there to receive or
+     * to deliver
+     * </ul>
+     *
+     * @return A HashMap containing the key/value pairs Trucks/LocalDateTime.
+     */
+    public static HashMap<Truck, LocalDateTime> read() {
+        InputStream inp;
+        HashMap<Truck, LocalDateTime> truckmap;
+
+        truckmap = new HashMap<Truck, LocalDateTime>();
+        try {
+            inp = new FileInputStream(FILEPATH);
+            Workbook wb = WorkbookFactory.create(inp);
+            Sheet sheet = wb.getSheetAt(0);
+            int rownum = 1;
+            Row row = sheet.getRow(rownum);
+            Cell cell1 = row.getCell(0);
+            Cell cell2 = row.getCell(1);
+            Cell cell3 = row.getCell(2);
+            String date, id, kind;
+            while (cell1 != null) {
+                date = cell1.getStringCellValue();
+                id = cell2.getStringCellValue();
+                kind = cell3.getStringCellValue();
+                LocalDateTime time = parseDateTime(date);
+                Truck t = new Truck(id, kind);
+                truckmap.put(t, time);
+                rownum++;
+                row = sheet.getRow(rownum);
+                cell1 = row.getCell(0);
+                cell2 = row.getCell(1);
+                cell3 = row.getCell(2);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        } catch (InvalidFormatException e) {
+            e.printStackTrace();
+            System.exit(1);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return truckmap;
+    }
+
+    /**
+     * ExcelTruckReader should not be instantiated, so the constructor is
+     * private.
+     */
+    private ExcelTruckReader() {
+
+    }
 }
---
+++ src/nl/jslob/tba/gatesim/util/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * The util package has ultility classes that provide some functionality that
+ * is not directly simulator related.
+ */
+package nl.jslob.tba.gatesim.util;
