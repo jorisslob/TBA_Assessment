e1e8608ea2f7717b11ff8811bdeb0a2bb066f5f3
Joris Slob <jorisslob@gmail.com>
2014-09-29 20:48:37 +0200

    Checkstyle applied to Transit

--- src/nl/jslob/tba/gatesim/components/Transit.java
+++ src/nl/jslob/tba/gatesim/components/Transit.java
@@ -3,24 +3,46 @@ package nl.jslob.tba.gatesim.components;
 import nl.jslob.tba.gatesim.simulator.Schedule;
 import nl.jslob.tba.gatesim.simulator.Truck;
 
+/**
+ * Transit is a component that simulates a road. The waiting time is fixed.
+ *
+ * @author jslob
+ *
+ */
 public class Transit implements Component {
 
-	Schedule schedule;
-	
-	public Transit(Schedule schedule) {
-		this.schedule = schedule;
-	}
-	
-	@Override
-	public void acceptTruck(Truck t) {
-		t.setLocation(this);
-		// All the roads in this simulation are 3 minutes long...
-		schedule.nextTruckSecondFromNow(t, 3 * 60);
-	}
+    /**
+     * WAITTIME for the Transit is fixed. All trucks spend 3 minutes in transit
+     * from one place to the other. If this needs to change, consider making it
+     * a paramter to the constructor.
+     */
+    private static final int WAITTIME = 3 * 60;
 
-	@Override
-	public void releaseTruck(Truck t) {
+    /**
+     * Link to the simulation scheduler. This is needed to request a callback.
+     */
+    private Schedule schedule;
 
-	}
+    /**
+     * Constructor of the Transit Component. Initializes the link to the
+     * schedule.
+     *
+     * @param schedule
+     *            Link to the simulator schedule
+     */
+    public Transit(final Schedule schedule) {
+        this.schedule = schedule;
+    }
+
+    @Override
+    public final void acceptTruck(final Truck t) {
+        t.setLocation(this);
+        schedule.nextTruckSecondFromNow(t, WAITTIME);
+    }
+
+    @Override
+    public void releaseTruck(final Truck t) {
+
+    }
 
 }
