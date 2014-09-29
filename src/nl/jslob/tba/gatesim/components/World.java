fe75b14fc257d26301c31e29f7f3a791cd0b9a7d
Joris Slob <jorisslob@gmail.com>
2014-09-29 21:45:58 +0200

    Checkstyle applied to World

--- src/nl/jslob/tba/gatesim/components/World.java
+++ src/nl/jslob/tba/gatesim/components/World.java
@@ -3,22 +3,42 @@ package nl.jslob.tba.gatesim.components;
 import nl.jslob.tba.gatesim.simulator.Statistics;
 import nl.jslob.tba.gatesim.simulator.Truck;
 
+/**
+ * The World Component is used twice in a simulation. Both as the place where
+ * trucks come from, the first component in the simulation and the place trucks
+ * will end up, the last components of the simulation. When a truck reaches this
+ * last state, it will report back its personal statistics to the Statistics
+ * Class.
+ *
+ * @author jslob
+ *
+ */
 public class World implements Component {
 
-	Statistics stats;
+    /**
+     * Link to the simulation Statistics object.
+     */
+    private Statistics stats;
 
-	public World(Statistics stats) {
-		this.stats = stats;
-	}
+    /**
+     * Constructor for the World component. It needs a link to the simulation
+     * Statistics object.
+     *
+     * @param stats
+     *            link to the simulation Statistic object.
+     */
+    public World(final Statistics stats) {
+        this.stats = stats;
+    }
 
-	@Override
-	public void acceptTruck(Truck t) {
-		stats.addTruck(t);
-	}
+    @Override
+    public final void acceptTruck(final Truck t) {
+        stats.addTruck(t);
+    }
 
-	@Override
-	public void releaseTruck(Truck t) {
+    @Override
+    public void releaseTruck(final Truck t) {
 
-	}
+    }
 
 }
