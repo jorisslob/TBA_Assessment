51825feb85a7591890ba1d28fb11da4cdfa8b526
Joris Slob <jorisslob@gmail.com>
2014-10-01 21:31:55 +0200

    Added statistical check to the GammaDistributionRate

--- src/test/java/nl/jslob/tba/gatesim/util/GammaDistributionRateTest.java
+++ src/test/java/nl/jslob/tba/gatesim/util/GammaDistributionRateTest.java
@@ -8,18 +8,27 @@ public class GammaDistributionRateTest {
     
     @Test
     public void testGammaDistributionRate() {
-        // This is a normal call to the Gamma Distribution Rate
+        // This is a normal call to the GammaDistributionRate
         @SuppressWarnings("unused")
         GammaDistributionRate gdr = new GammaDistributionRate(9,3);
     }
 
     @Test
     public void testGetValueInSeconds() {
+        // Get some values from the GammaDistributionRate and check
+        // properties:
+        // * All values should be >0
+        // * The average should tend towards 180
+        int tests = 1000;
         GammaDistributionRate gdr = new GammaDistributionRate(9,3);
-        for(int i=0; i<1000; i++) {
+        double avg = 0.0;
+        for(int i=0; i<tests; i++) {
             long s = gdr.getValueInSeconds();
-            assertTrue(s>=0);
+            avg += s/(double) tests;
+            assertTrue("Value is less than 0", s>0);
         }
+        double expected = 3*60;
+        double diff = Math.abs(avg-expected);
+        assertTrue("Average "+avg+" too far from expected value "+expected, Math.abs(avg-180.0)<5.0);
     }
-
 }
